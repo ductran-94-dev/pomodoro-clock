a16fb629478db1d3149b332bcbdfd4503118dd31
Duc Tran Minh <ductm@devblock.net>
2021-03-03 22:54:40 +0700

    Update manifest

--- internals/webpack/webpack.prod.babel.js
+++ internals/webpack/webpack.prod.babel.js
@@ -116,11 +116,11 @@ module.exports = require('./webpack.base.babel')({
     }),
 
     new WebpackPwaManifest({
-      name: 'React Boilerplate',
-      short_name: 'React BP',
-      description: 'My React Boilerplate-based project!',
+      name: 'Pomodoro Clock',
+      short_name: 'Pomodoro Clock',
+      description: 'My Pomodoro Clock project!',
       background_color: '#fafafa',
-      theme_color: '#b1624d',
+      theme_color: '#385898',
       inject: true,
       ios: true,
       icons: [
